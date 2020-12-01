b4d36aefe946aed50776fe4b3b5af28342839f0b
Niklas Baerveldt <niklas.baerveldt@gmail.com>
2020-12-01 17:54:05 +0100

    Added a graph showing the number of humans over time, some zombies seems to dissappear

--- alternativeZombie/Main.m
+++ alternativeZombie/Main.m
@@ -3,18 +3,41 @@ gridSize = 50;
 nrOfHumans = 50;
 nrOfZombies = 1;
 nrOfObstacles = 20;
+stopTime = 500;
+XEdge = stopTime;
+YEdge = nrOfHumans;
 zombieStepLength = 1;
 %humanStepLength = 2;
-
+nrHumansOverTime = zeros(1,stopTime);
+nrZombiesOverTime = zeros(1,stopTime);
 [humans,zombies,obstacles] = InitializePopulation(gridSize,nrOfHumans,nrOfZombies,nrOfObstacles);
-
-timePeriod = 1 : 500;
+figure1 = figure(1);
+figure2 = figure(2);
+timePeriod = 1 : stopTime;
+time = 1;
 
 while sum(sum(humans==1)) > 0
     zombies = MoveZombies(humans,zombies,obstacles,zombieStepLength);
     [humans,zombies] = InfectHumans(humans,zombies);
-    %humans = MoveHumans(humans,zombies,humanStepLength); 
+    %humans = MoveHumans(humans,zombies,humanStepLength);
+    set(0,'CurrentFigure',figure1);
     VisualizePopulation(humans,zombies,obstacles)
+    nrHumansOverTime(time) = sum(sum(humans));
+    nrZombiesOverTime(time) = sum(sum(zombies));
+    
+     set(0,'CurrentFigure',figure2);
+        cla
+        hold on
+        %axis([0,XEdge,0,YEdge])
+        plot(nrHumansOverTime(1:time),'b-')
+        %plot(nrZombiesOverTime(1:time),'r-') uncomment this to see
+        %dissappearing zombie phenomenom
+        
+        %Cheatcode for demonstration:
+        plot(50-nrHumansOverTime(1:time),'r-')
+        hold off
+        drawnow update
+    time = time + 1;
 end
 
 
