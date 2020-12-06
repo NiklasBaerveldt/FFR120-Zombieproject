dd387827aebcaddb6539a579bceec391d89810df
Kostas234 <66828119+Kostas234@users.noreply.github.com>
2020-12-06 16:42:30 +0100

    Obstacle and human in same position issue fixed
    Obstacle and human in same position issue fixed

--- alternativeZombie/GenerateGridCoordinates.m
+++ alternativeZombie/GenerateGridCoordinates.m
@@ -1,6 +1,19 @@
-function [xCoordinates,yCoordinates] = GenerateGridCoordinates(gridSize,nrOfPoints)
-gridDimension = [gridSize, gridSize];
-[xCoordinates,yCoordinates] = ind2sub(gridDimension,randperm(prod(gridDimension)));
-xCoordinates = xCoordinates(1:nrOfPoints); yCoordinates(1:nrOfPoints);
+function [xCoordinates,yCoordinates,areaGrid] = GenerateGridCoordinates(gridSize,nrOfPoints,areaGrid)
+  gridDimension = [gridSize, gridSize];
+  [xCoordinates,yCoordinates] = ind2sub(gridDimension,randperm(prod(gridDimension)));
+  for i = 1:gridSize
+    if areaGrid(xCoordinates(i),yCoordinates(i))==1
+      %disp(xCoordinates(i));
+      %disp(yCoordinates(i));
+      xCoordinates(i) = [];
+      yCoordinates(i) = [];
+    end
+  end
+  
+  xCoordinates = xCoordinates(1:nrOfPoints);
+  yCoordinates = yCoordinates(1:nrOfPoints);
+  for k = 1:nrOfPoints
+    areaGrid(xCoordinates(k),yCoordinates(k))=1;
+  end
 end
 
--- alternativeZombie/InitializePopulation.m
+++ alternativeZombie/InitializePopulation.m
@@ -3,31 +3,35 @@ function [humans,zombies,obstacles] = InitializePopulation(gridSize,nrOfHumans,n
   humans = zeros(gridSize); 
   zombies = zeros(gridSize);
   obstacles = zeros(gridSize);
-  obstacleSize = 10;
-  [humanXCoordinates,humanYCoordinates] = GenerateGridCoordinates(gridSize,nrOfHumans);
-  [zombieXCoordinates,zombieYCoordinates] = GenerateGridCoordinates(gridSize,nrOfZombies); % Maybe not random?
-  [obstacleXCoordinates,obstacleYCoordinates] = GenerateGridCoordinates(gridSize,nrOfObstacles); % Maybe not random?
+  areaGrid = zeros(gridSize);
+  obstacleSize = 2;
+  [humanXCoordinates,humanYCoordinates,areaGrid] = GenerateGridCoordinates(gridSize,nrOfHumans,areaGrid);
+  [zombieXCoordinates,zombieYCoordinates,areaGrid] = GenerateGridCoordinates(gridSize,nrOfZombies,areaGrid);
+  [obstacleXCoordinates,obstacleYCoordinates,areaGrid] = GenerateGridCoordinates(gridSize,nrOfObstacles,areaGrid);
 
   for i = 1 : nrOfHumans
     humans(humanXCoordinates(i),humanYCoordinates(i)) = 1;
   end
-
+      
   for i = 1:nrOfZombies
     zombies(zombieXCoordinates(i),zombieYCoordinates(i)) = 1;
   end
-
+  
   for i = 1:nrOfObstacles
     obstacles(obstacleXCoordinates(i),obstacleYCoordinates(i)) = 1;
-    r = randi([0 1],1);
-    x = randi([-1 1],1)*(1-r);
-    y = randi([-1 1],1)*r;
+    %r = randi([0 1],1);
+    %x = randi([-1 1],1);%*(1-r);
+    %y = randi([-1 1],1);%*r;
     for j = 1:obstacleSize
+      x = randi([-1 1],1);%*(1-r);
+      y = randi([-1 1],1);%*r;
       if obstacleXCoordinates(i)+x<gridSize+1 && obstacleXCoordinates(i)+x>0 ...
-      && obstacleYCoordinates(i)+y<gridSize+1 && obstacleYCoordinates(i)+y>0
+      && obstacleYCoordinates(i)+y<gridSize+1 && obstacleYCoordinates(i)+y>0 ...
+      && areaGrid(obstacleXCoordinates(i)+x,obstacleYCoordinates(i)+y)~=1
         obstacles(obstacleXCoordinates(i)+x,obstacleYCoordinates(i)+y)=1;
       end
     end
   end
-
+ 
 end
 
