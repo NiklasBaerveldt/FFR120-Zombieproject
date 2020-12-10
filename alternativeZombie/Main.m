1443ec446a66458c5ef1c993cf0518a3f06f3717
Andab <74553040+Andab@users.noreply.github.com>
2020-12-10 16:44:31 +0100

    Add files via upload
    Added an implementation to be able to change the stepLength for both the human and the zombies

--- alternativeZombie/Main.m
+++ alternativeZombie/Main.m
@@ -1,19 +1,25 @@
 clc; clear;
 gridSize = 50;
-nrOfHumans = 5;
+nrOfHumans = 50;
 nrOfZombies = 1;
-nrOfObstacles = 10;
-zombieStepLength = 1;
-%humanStepLength = 2;
-sightRange = 20;
+nrOfObstacles = 30;
+zombieStepLength = 2;
+humanStepLength = 1;
+sightRange = 10;
 [humans,zombies,obstacles] = InitializePopulation(gridSize,nrOfHumans,nrOfZombies,nrOfObstacles);
-timePeriod = 1 : 500;
+
+
 
 while sum(sum(humans==1)) > 0
+    for t = 1 : zombieStepLength
     zombies = MoveZombies(humans,zombies,obstacles,1,sightRange);
+    end
     [humans,zombies] = InfectHumans(humans,zombies);
+    for t = 1 : humanStepLength
     humans = MoveHumans(humans,obstacles,zombies,sightRange);
+    end
     VisualizePopulation(humans,zombies,obstacles)
+    sum(sum(humans==1))
 end
 
 
